bda6ae3e5447c5b95e46e9a861232cb2b7c4b256
Muhammed Sibly <tech.sibly@gmail.com>
2024-04-18 13:14:00 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:sample_app/home.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 50, 50, 50),
+        title: Text(
+          'Home Page',
+          style: TextStyle(
+            color: Colors.white, // Set the font color to white
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 10),
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage('assets/images/man-with-phone.png'),
+                  // height: 100, // Adjust height as needed
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Fluttertoast.showToast(
+                    //     msg: "Login Successful",
+                    //     toastLength: Toast.LENGTH_SHORT,
+                    //     gravity: ToastGravity.BOTTOM,
+                    //     timeInSecForIosWeb: 1,
+                    //     backgroundColor: Color.fromARGB(255, 54, 50, 50),
+                    //     textColor: Color.fromARGB(255, 255, 255, 255),
+                    //     fontSize: 16.0);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                    // Handle login logic here
+                  },
+                  child: Text('Login'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:sample_app/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  void _login(BuildContext context) {
+    // Perform login logic here
+    // For demo purposes, directly navigate to Home page
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Login Successful'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/man-with-phone.png',
+                        height: 300, // Adjust height as needed
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 300, // Limit width for web view
+                        child: TextField(
+                          decoration: const InputDecoration(labelText: 'Email'),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: 300, // Limit width for web view
+                        child: TextField(
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          _login(context);
+                        },
+                        child: const Text('Login'),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              // Handle "Forgot Password?" action
+                            },
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 96, 33, 243)),
+                            ),
+                          ),
+                          const Text('|'),
+                          TextButton(
+                            onPressed: () {
+                              // Handle "Sign up" action
+                            },
+                            child: const Text(
+                              'Sign up',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 96, 33, 243)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
